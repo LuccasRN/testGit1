c39ef52a9ddcb631114ca5767c41347297cd52c6
LucasRD26 <238353319+theogon2002-sudo@users.noreply.github.com>
2025-10-15 17:42:27 -0500

    comment mod

--- demo/src/main/java/test/Main.java
+++ demo/src/main/java/test/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //comment Test modification
+        //comment Test modification 2(fait par voisin)
     }
 }
